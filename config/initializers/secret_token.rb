6388bdd118b9c2423bf09b73b8b73f06eba2241b
Alex Propes <alex.propes@gmail.com>
2012-12-07 13:21:46 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govtwits::Application.config.secret_token = 'b697130ae45de3656d498d390d3567354b146f4a5dff7184da476368f27cdbbfee709b22c5b82568ea6914ff49358a29df573d192be3b90470b323f08fe5a5e0'
